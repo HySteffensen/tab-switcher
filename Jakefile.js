6a9a4ddb4001ae558f2f2a27bcc8372229c6b397
HySteffensen <hyrum.steffensen@gmail.com>
2018-06-04 12:14:42 -0700

    configured linter

--- Jakefile.js
+++ Jakefile.js
@@ -1,10 +1,56 @@
+/* globals jake:false, desc:false, task:false, complete:false, fail:false, directory:false */
 
+"use strict";
 
-
+var jshint = require("simplebuild-jshint");
 
 
 desc("Default Task");
-task("default", [], function() {
+task("default", ["lint"], function() {
     console.log("\n\nBUILD OK");
 });
 
+desc("Lints All the Code");
+task("lint", function() {
+    process.stdout.write("Linting JavaScript: ");
+
+    jshint.checkFiles({
+        files: [ "Jakefile.js", "./src/**/*.js", "./spikes/**/*.js" ],
+        options: lintOptions(),
+        globals: lintGlobals()
+    }, complete, fail);
+}, { async: true });
+
+function lintOptions() {
+    return {
+        bitwise: true,
+        eqeqeq: true,
+        forin: true,
+        freeze: true,
+        futurehostile: true,
+        latedef: "nofunc",
+        noarg: true,
+        nocomma: true,
+        nonbsp: true,
+        nonew: true,
+        strict: true,
+        undef: true,
+
+        node: true,
+        browser: true,
+
+        esversion: 6
+    };
+}
+
+function lintGlobals() {
+    return {
+        // Mocha
+        describe: false,
+        it: false,
+        before: false,
+        after: false,
+        beforeEach: false,
+        afterEach: false
+    };
+}
